f76ebc3778599c6b3d2d6c12ee8639153b832a94
danh <phanthanhdanh7108@gmail.com>
2026-08-31 21:53:17 +0700

    fix(migration): make AddMaDonViToMauBangKhen migration idempotent with IF NOT EXISTS to prevent error 2705

--- Backend/Migrations/20260831143645_AddMaDonViToMauBangKhen.cs
+++ Backend/Migrations/20260831143645_AddMaDonViToMauBangKhen.cs
@@ -10,105 +10,50 @@ namespace Backend.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "ghi_chu",
-                schema: "dbo",
-                table: "TienDoBaiHoc",
-                type: "nvarchar(max)",
-                nullable: true);
+            migrationBuilder.Sql(@"
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('dbo.MauBangKhen') AND name = 'ma_don_vi')
+BEGIN
+    ALTER TABLE [dbo].[MauBangKhen] ADD [ma_don_vi] INT NULL;
+    EXEC('UPDATE [dbo].[MauBangKhen] SET [ma_don_vi] = 1 WHERE [ma_don_vi] IS NULL;');
+END
 
-            migrationBuilder.AddColumn<int>(
-                name: "ma_job_nguon",
-                schema: "dbo",
-                table: "ThoiKhoaBieu",
-                type: "int",
-                nullable: true);
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_MauBangKhen_ma_don_vi' AND object_id = OBJECT_ID('dbo.MauBangKhen'))
+BEGIN
+    CREATE INDEX [IX_MauBangKhen_ma_don_vi] ON [dbo].[MauBangKhen]([ma_don_vi]);
+END
 
-            migrationBuilder.AddColumn<int>(
-                name: "so_xung_dot_cung",
-                schema: "dbo",
-                table: "ScheduleGenerationJob",
-                type: "int",
-                nullable: true);
+IF NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = 'FK_MauBangKhen_ma_don_vi__DonVi')
+BEGIN
+    ALTER TABLE [dbo].[MauBangKhen] ADD CONSTRAINT [FK_MauBangKhen_ma_don_vi__DonVi] 
+    FOREIGN KEY ([ma_don_vi]) REFERENCES [dbo].[DonVi]([ma_don_vi]);
+END
 
-            migrationBuilder.AddColumn<int>(
-                name: "ma_don_vi",
-                schema: "dbo",
-                table: "MauBangKhen",
-                type: "int",
-                nullable: true);
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('dbo.TienDoBaiHoc') AND name = 'ghi_chu')
+BEGIN
+    ALTER TABLE [dbo].[TienDoBaiHoc] ADD [ghi_chu] NVARCHAR(MAX) NULL;
+END
 
-            migrationBuilder.Sql("UPDATE [dbo].[MauBangKhen] SET [ma_don_vi] = 1 WHERE [ma_don_vi] IS NULL;");
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('dbo.ThoiKhoaBieu') AND name = 'ma_job_nguon')
+BEGIN
+    ALTER TABLE [dbo].[ThoiKhoaBieu] ADD [ma_job_nguon] INT NULL;
+END
 
-            migrationBuilder.CreateTable(
-                name: "MonHocChuyenNganh",
-                schema: "dbo",
-                columns: table => new
-                {
-                    ma_mon_hoc = table.Column<int>(type: "int", nullable: false),
-                    ma_chuyen_nganh = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_MonHocChuyenNganh", x => new { x.ma_mon_hoc, x.ma_chuyen_nganh });
-                    table.ForeignKey(
-                        name: "FK_MonHocChuyenNganh_ma_chuyen_nganh__ChuyenNganh",
-                        column: x => x.ma_chuyen_nganh,
-                        principalSchema: "dbo",
-                        principalTable: "ChuyenNganh",
-                        principalColumn: "ma_chuyen_nganh",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_MonHocChuyenNganh_ma_mon_hoc__DanhMucMonHoc",
-                        column: x => x.ma_mon_hoc,
-                        principalSchema: "dbo",
-                        principalTable: "DanhMucMonHoc",
-                        principalColumn: "ma_mon_hoc",
-                        onDelete: ReferentialAction.Cascade);
-                });
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('dbo.ScheduleGenerationJob') AND name = 'so_xung_dot_cung')
+BEGIN
+    ALTER TABLE [dbo].[ScheduleGenerationJob] ADD [so_xung_dot_cung] INT NULL;
+END
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ThoiKhoaBieu_ma_job_nguon",
-                schema: "dbo",
-                table: "ThoiKhoaBieu",
-                column: "ma_job_nguon");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MauBangKhen_ma_don_vi",
-                schema: "dbo",
-                table: "MauBangKhen",
-                column: "ma_don_vi");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MonHocChuyenNganh_MaChuyenNganh",
-                schema: "dbo",
-                table: "MonHocChuyenNganh",
-                column: "ma_chuyen_nganh");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MonHocChuyenNganh_MaMonHoc",
-                schema: "dbo",
-                table: "MonHocChuyenNganh",
-                column: "ma_mon_hoc");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_MauBangKhen_ma_don_vi__DonVi",
-                schema: "dbo",
-                table: "MauBangKhen",
-                column: "ma_don_vi",
-                principalSchema: "dbo",
-                principalTable: "DonVi",
-                principalColumn: "ma_don_vi");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ThoiKhoaBieu_ma_job_nguon__ScheduleGenerationJob",
-                schema: "dbo",
-                table: "ThoiKhoaBieu",
-                column: "ma_job_nguon",
-                principalSchema: "dbo",
-                principalTable: "ScheduleGenerationJob",
-                principalColumn: "ma_job",
-                onDelete: ReferentialAction.SetNull);
+IF OBJECT_ID('dbo.MonHocChuyenNganh', 'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[MonHocChuyenNganh] (
+        [ma_mon_hoc] INT NOT NULL,
+        [ma_chuyen_nganh] INT NOT NULL,
+        CONSTRAINT [PK_MonHocChuyenNganh] PRIMARY KEY ([ma_mon_hoc], [ma_chuyen_nganh]),
+        CONSTRAINT [FK_MonHocChuyenNganh_ma_chuyen_nganh__ChuyenNganh] FOREIGN KEY ([ma_chuyen_nganh]) REFERENCES [dbo].[ChuyenNganh]([ma_chuyen_nganh]) ON DELETE CASCADE,
+        CONSTRAINT [FK_MonHocChuyenNganh_ma_mon_hoc__DanhMucMonHoc] FOREIGN KEY ([ma_mon_hoc]) REFERENCES [dbo].[DanhMucMonHoc]([ma_mon_hoc]) ON DELETE CASCADE
+    );
+END
+");
         }
 
         /// <inheritdoc />
